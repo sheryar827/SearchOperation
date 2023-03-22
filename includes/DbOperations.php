4db1e95cb949acd1a9e91f07b28ebf2723ec7574
Sheryar Kiani <ntiscs6@gmail.com>
2023-03-22 00:34:14 -0700

    commit 2

--- includes/DbOperations.php
+++ includes/DbOperations.php
@@ -3,9 +3,9 @@
     class DbOperations{
         //private $con;
 		
-		private $conAndroid;
-        private $conSubDB;
-        private $conForensic;
+		//private $conAndroid;
+        //private $conSubDB;
+        //private $conForensic;
         private $conPS;
         function __construct(){
 			
@@ -13,9 +13,9 @@
 			
 			$db = new DbConnect();
 			
-			$this->conSubDB = $db->connect();
-			$this->conAndroid = $db->connectAndroid();
-            $this->conForensic = $db->connectForensic();
+			//$this->conSubDB = $db->connect();
+			//$this->conAndroid = $db->connectAndroid();
+            //$this->conForensic = $db->connectForensic();
             $this->conPS = $db->connectPS();
         }
 
@@ -90,38 +90,238 @@
         // create new user in database
         public function uploadData($ps, 
         $lat, 
-        $lng, 
+        $lng,
+        $name, 
         $cnic, 
         $msisdn,
         $image,
-        $status){
+        $status,
+        $mobId){
            
 			$sql = "EXEC [sp_searchOps_searchdata_uploaddata] 
             @PoliceStation = ?, 
             @Lat = ?, 
             @Lng = ?,
+            @Name = ?,
             @Cnic = ?,
             @Msisdn = ?,
             @Image = ?,
-            @Status = ?";
+            @Status = ?,
+            @MobileId = ?";
 			$params = array($ps, 
             $lat, 
             $lng, 
+            $name,
             $cnic, 
             $msisdn, 
             $image,
-            $status);
+            $status,
+            $mobId);
 
 			$getResults= sqlsrv_query($this->conPS, $sql, $params);
 			
 
 			if ($getResults == FALSE){
+                die(print_r( sqlsrv_errors(), true));
 				return false;
 			}else{
 				return true;
 			}
 			sqlsrv_free_stmt($getResults);
             }
+
+
+            public function getUserByMobileId($mobileId){
+                $sql = "EXEC sp_searchops_users_isuserexist @MobileId = ?";
+                $params = array($mobileId);
+                $stmt= sqlsrv_query($this->conPS, $sql, $params);
+                if (sqlsrv_has_rows( $stmt ) === false){
+                    echo "Error in retrieveing row count.";
+                }
+                else{
+                    $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+                    return $row;
+                }
+                
+            }
+
+            // create new user in database
+        public function createUser($UserName, 
+        $MobileId, 
+        $Msisdn, 
+        $Cnic, 
+        $Password,
+        $SecEncKey,
+        $FCM){
+            $password = password_hash($Password, PASSWORD_DEFAULT);
+            if(!$this -> isUserExist($MobileId)){
+            $description = 'UserName: '.$UserName.' Password: '.$Password;
+			
+			//$date = date('Y-m-d H:i:s');
+			// EXEC the procedure, {call stp_Create_Item (@Item_ID = ?, @Item_Name = ?)} seems to fail with various errors in my experiments
+			$sql = "EXEC sp_searchops_users_insertuser 
+            @UserName = ?, 
+            @Password = ?, 
+            @MobileId = ?,
+            @Msisdn = ?,
+            @Cnic = ?,
+            @Description = ?,
+            @SecEncKey = ?,
+            @FCM = ?";
+			$params = array($UserName, 
+            $password, 
+            $MobileId, 
+            $Msisdn, 
+            $Cnic, 
+            $description,
+            $SecEncKey,
+            $FCM);
+			$getResults= sqlsrv_query($this->conPS, $sql, $params);
+			//$rowsAffected = sqlsrv_rows_affected($getResults);
+			if ($getResults == FALSE){
+				die(print_r( sqlsrv_errors(), true));
+				return USER_FAILURE;
+			}else{
+				return USER_CREATED;
+			}
+			sqlsrv_free_stmt($getResults);
+            }
+
+            return USER_EXIST;
+		}
+
+
+        // check user login
+        public function userLogin($username, $mobId, $pass){
+			//$valid = false;
+            if($this -> isUserExist($mobId)){
+                $user = array();
+                $user = $this->getUserByMobileId($mobId);
+                if(password_verify($pass, $user['Password']) && $user['IsActive'] == true
+				|| ($user['Password'] == 'demouser'))	{
+					//$valid = true;
+					
+					/*StoredProcedure to insert user login time to login time table*/
+					$sql = "EXEC sp_searchops_userlogintime_insertrecord 
+                    @UserName = ?,
+                    @UserMobileId = ?,
+                    @UserMsisdn = ?,
+                    @UserCnic = ?";
+					$params = array($username,
+                                    $mobId,
+                                    $user['Msisdn'],
+                                    $user['Cnic']);
+					$getResults = sqlsrv_query($this->conPS, $sql, $params);
+                    
+					if ($getResults == FALSE){
+                        die(print_r( sqlsrv_errors(), true));
+                        return USER_FAILURE;
+                    }else{
+                        return USER_AUTHENTICATED;;
+                    }
+                    sqlsrv_free_stmt($getResults);
+                    
+            }else{
+                return USER_NOT_AUTHENTICATED;
+            }		
+                
+                //sqlsrv_free_stmt($getResults);
+        }else{
+            return USER_NOT_FOUND;
         }
+			//return $valid;
+		}
+
+        // check user exist
+        private function isUserExist($mobileId){
+            $sql = "EXEC sp_searchops_users_isuserexist @MobileId = ?";
+			$params = array($mobileId);
+	        $stmt= sqlsrv_query($this->conPS, $sql, $params);
+            return sqlsrv_has_rows( $stmt );
+	       
+        }
+
+        public function isAlreadyPresent($cnic, $msisdn){
+            $sql = "EXEC sp_searchops_person_ispersonexist @Cnic = ?,
+            @Msisdn = ?";
+			$params = array($cnic,
+            $msisdn);
+	        $stmt= sqlsrv_query($this->conPS, $sql, $params);
+            return sqlsrv_has_rows( $stmt );
+        }
+
+        public function getRoles(){
+            $result = array();
+            $sql = "EXEC [sp_searchops_roles_getroles]";
+	        $getResults= sqlsrv_query($this->conPS, $sql);
+			if (sqlsrv_has_rows( $getResults ) === false){
+				echo "Error in retrieveing row count.";
+			}
+			else{
+				
+			    while( $row = sqlsrv_fetch_array($getResults) ) {
+			     array_push($result,
+			    array('roleid'=>$row[0],'roletitle'=>$row[1]));
+			    }
+			}
+
+            return $result;
+        }
+
+        public function generateRandomString() {
+
+            $length = 32;
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $charactersLength = strlen($characters);
+            $randomString = '';
+            for ($i = 0; $i < $length; $i++) {
+                $randomString .= $characters[rand(0, $charactersLength - 1)];
+            }
+            return $randomString;
+        }
+
+
+        public function activateUser($mobileId, $RoleId){
+            $sql = "EXEC [sp_searchops_users_activateuser] @MobileId = ?, @RoleId = ?";
+			$params = array($mobileId, $RoleId);
+	        $getResults= sqlsrv_query($this->conPS, $sql, $params);
+			if ($getResults === false) {
+                die( print_r( sqlsrv_errors(), true) );
+                return USER_NOT_ACTIVATED;
+            }
+            else{
+                return USER_ACTIVATED;
+            }
+              
+              sqlsrv_free_stmt($getResults);
+        }
+
+
+        public function inActiveUsersList(){
+            //if($this->userLogin($username, $mobId, $pass)){
+            //$sql = "SELECT [UserName], [isActive] FROM [XMatchUM].[dbo].[Users] where [isActive] = 1";
+            $sql = "EXEC [sp_searchops_users_inactiveuserslist]";
+            $getResults= sqlsrv_query($this->conPS, $sql);
+            if( $getResults === false) {
+            die( print_r( sqlsrv_errors(), true) );
+            }
+            $result = array();
+            while( $row = sqlsrv_fetch_array($getResults) ) {
+              array_push($result, 
+              array('username' =>$row[0]
+              , 'msisdn' => $row[1]
+              , 'cnic' => $row[2]
+              , 'mobId' => $row[3]));
+            }
+            
+            //return json_encode(array("users"=>$result));
+            return $result;
+            sqlsrv_free_stmt($getResults);
+        //}
+            
+        }
+
+
+    }
     
 ?>        
--- public/index.php
+++ public/index.php
@@ -6,8 +6,244 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 require '../includes/DbOperations.php';
+require '../includes/ChCrytpo.php';
 
 $app = AppFactory::create();
+
+
+$app->post('/SearchOperation/Public/Createuser', function (Request $request, Response $response) {
+    if(!haveEmptyParameters(array('UserName', 
+	'MobileId',
+	'UserCNIC',
+	'UserMSISDN',
+	'Password', 
+	'EncKey',
+	'FCM'), $response)){
+
+
+		$db = new DbOperations;
+		$chcrytpo = new ChCrytpo;
+
+	$request_data = $request->getParsedBody();
+
+	$EncKey = $request_data['EncKey'];
+
+	// Original EncKey hidden inside Id
+	$SecKey = $chcrytpo->decrypt($request_data['Id']
+	, strrev($EncKey));
+
+
+
+	$UserName = $chcrytpo->decrypt($request_data['UserName']
+	, $EncKey);
+	$MobileId = $chcrytpo->decrypt($request_data['MobileId']
+	, $EncKey);
+	$UserCNIC = $chcrytpo->decrypt($request_data['UserCNIC']
+	, $EncKey);
+	$UserMSISDN = $chcrytpo->decrypt($request_data['UserMSISDN']
+	, $EncKey);
+	$Password = $chcrytpo->decrypt($request_data['Password']
+	, $EncKey);
+
+	$FCM = $chcrytpo->decrypt($request_data['FCM']
+	, $EncKey);
+	
+
+	$db = new DbOperations;
+
+	$result = $db->createUser($UserName,
+	$MobileId,
+	$UserMSISDN,
+	$UserCNIC,
+	$Password,
+	$SecKey,
+	$FCM);
+
+	if($result == USER_CREATED ){
+		
+		$message = array();
+		$message['error'] = false;
+		$message['message'] = $chcrytpo->encrypt('User created successfully'
+		,$EncKey); 
+		
+
+		$response->getBody()->write(json_encode($message));
+		return $response
+					->withHeader('Content-type', 'application/json')
+					->withStatus(200);
+	}
+	else if($result == USER_FAILURE){
+		$message = array();
+		$message['error'] = true;
+		$message['message'] = $chcrytpo->encrypt('Some error occured'
+		,$EncKey); 
+
+		$response->getBody()->write(json_encode($message));
+		return $response
+					->withHeader('Content-type', 'application/json')
+					->withStatus(200);
+	}
+	else if ($result == USER_EXIST) {
+		$message = array();
+		$message['error'] = true;
+		$message['message'] =  $chcrytpo->encrypt('Some error occured'
+		,$EncKey); 
+
+		$response->getBody()->write(json_encode($message));
+		return $response
+					->withHeader('Content-type', 'application/json')
+					->withStatus(200);
+	}
+}
+return $response
+->withHeader('Content-type', 'application/json')
+->withStatus(422);
+});
+
+$app->post('/SearchOperation/Public/Loginuser', function (Request $request, Response $response) {
+	
+	$db = new DbOperations;
+	$chcrytpo = new ChCrytpo;
+
+	if(!haveEmptyParameters(array('UserName', 
+	'MobileId', 
+	'Password', 
+	'EncKey'), $response)){
+
+		$request_data = $request->getParsedBody();
+
+		
+
+		$EncKey = $request_data['EncKey'];
+
+
+		$MobileId =$chcrytpo->decrypt($request_data['MobileId']
+		, $EncKey); 
+
+		$user = $db->getUserByMobileId($MobileId);
+
+		$UserName = $chcrytpo->decrypt($request_data['UserName']
+		, $user['EncKey']); 
+		
+		$Password = $chcrytpo->decrypt($request_data['Password']
+		, $user['EncKey']); 
+		
+
+		// $UserName = $request_data['UserName'];
+		// $MobileId = $request_data['MobileId'];
+		// $Password = $chcrytpo->decrypt($request_data['Password']
+		// , $EncKey); 
+
+
+		$result = $db->userLogin($UserName, $MobileId, $Password);
+
+
+		if($result == USER_AUTHENTICATED){
+
+			$response_data = array();
+			//$user = $db->getUserByMobileId($MobileId);
+			$response_data['error'] = false;
+			//$response_data['message'] = 'Login Successful';
+			$response_data['message'] = $chcrytpo->encrypt('Login Successful'
+			, $user['EncKey']);
+			$response_data['role'] = $chcrytpo->encrypt($user['RoleId']
+			, $user['EncKey']);
+
+			$response->getBody()->write(json_encode($response_data));
+			return $response
+					->withHeader('Content-type', 'application/json')
+					->withStatus(200);
+
+		}else if($result == USER_NOT_FOUND){
+			
+			$response_data = array();
+			
+			$response_data['error'] = true;
+			$response_data['message'] = 'User Not Exist';
+		
+
+			
+
+			$response->getBody()->write(json_encode($response_data));
+			return $response
+					->withHeader('Content-type', 'application/json')
+					->withStatus(200);
+
+		}else if($result == USER_NOT_AUTHENTICATED){
+			$response_data = array();
+			
+			$response_data['error'] = true;
+			$response_data['message'] = 'Invalid Credential';
+
+			$response->getBody()->write(json_encode($response_data));
+			return $response
+					->withHeader('Content-type', 'application/json')
+					->withStatus(200);
+		}
+	}
+
+	return $response
+					->withHeader('Content-type', 'application/json')
+					->withStatus(422);
+});
+
+$app->post('/SearchOperation/Public/GetUserNamePassword', function (Request $request, Response $response){
+	
+
+	$db = new DbOperations;
+		$chcrytpo = new ChCrytpo;
+
+	$request_data = $request->getParsedBody();
+
+	$EncKey = $request_data['EncKey'];
+
+	// Original EncKey hidden inside Id
+	$MobileId = $chcrytpo->decrypt($request_data['MobileId']
+	, $EncKey);
+
+	$user = $db->getUserByMobileId($MobileId);
+
+	// User Found
+	if(sizeof($user) > 0){
+		$UserName = $chcrytpo->encrypt($user['UserName']
+		, $EncKey); 
+		
+		// Get Password From Description
+		$pass = substr($user['Description'], strpos($user['Description']
+		, 'Password: ') + strlen('Password: '));
+
+		$Password = $chcrytpo->encrypt($pass
+		, $EncKey);
+		
+		$SecEnckKey = $chcrytpo->encrypt($user['EncKey']
+		, strrev($EncKey));
+
+		$response_data = array();
+		$response_data['UserName'] =  $UserName;
+		$response_data['Password'] = $Password;
+		$response_data['SecEncKey'] = $SecEnckKey;
+		$response->getBody()->write(json_encode(array('error'=>false, 'message'=>
+		$chcrytpo->encrypt(json_encode($response_data)
+	, strrev($EncKey)))));
+			return $response
+					->withHeader('Content-type', 'application/json')
+					->withStatus(200);
+		
+
+	}else{
+
+		$response->getBody()->write(json_encode(array('error'=>true, 'message'=>
+		'No Record Found!')));
+			return $response
+					->withHeader('Content-type', 'application/json')
+					->withStatus(200);
+
+	}
+
+	return $response
+					->withHeader('Content-type', 'application/json')
+					->withStatus(422);
+});
 
 
 $app->post('/SearchOperation/Public/UploadSearchOps', function (Request $request, Response $response) {
@@ -20,10 +256,12 @@ $app->post('/SearchOperation/Public/UploadSearchOps', function (Request $request
 	$ps = $request_data['ps'];
 	$lat = $request_data['lat'];
 	$lng = $request_data['lng'];
+	$name = $request_data['name'];
 	$cnic = $request_data['cnic'];
 	$msisdn = $request_data['msisdn'];
 	$image = $request_data['image'];	
 	$status = $request_data['status'];
+	$mobid = $request_data['mobId'];
 	
 
 	$db = new DbOperations;
@@ -31,10 +269,12 @@ $app->post('/SearchOperation/Public/UploadSearchOps', function (Request $request
 	$result = $db->uploadData($ps,
 	$lat,
 	$lng,
+	$name,
 	$cnic,
 	$msisdn,
 	$image,
-	$status);
+	$status,
+	$mobid);
 
 	if($result == true){
 		$message = array();
@@ -57,6 +297,9 @@ $app->post('/SearchOperation/Public/UploadSearchOps', function (Request $request
 					->withHeader('Content-type', 'application/json')
 					->withStatus(200);
 	}
+	return $response
+					->withHeader('Content-type', 'application/json')
+					->withStatus(422);
 }
 );
 
@@ -99,6 +342,143 @@ $app->get('/SearchOperation/Public/GetSearchData', function (Request $request, R
 
     return $response;
 });
+
+$app->get('/SearchOperation/Public/GetRoles', function(Request $request, Response $response){
+	$request_data = $request->getParsedBody();
+	$chcrytpo = new ChCrytpo;
+	//$cnic = $request_data['cnic'];
+	$db = new DbOperations;
+
+	$roles = $db->getRoles();
+
+	$EncKey = $db->generateRandomString();
+
+	$response_data['roles'] = $roles;
+
+	$response->getBody()->write(json_encode(array('error'=>false
+	, 'message'=>
+		$chcrytpo->encrypt(json_encode($response_data)
+	, $EncKey)
+	, 'enckey'=> $EncKey)));
+			return $response
+					->withHeader('Content-type', 'application/json')
+					->withStatus(200);
+});
+
+
+$app->post('/SearchOperation/Public/ActivateUser', function (Request $request, Response $response) {
+    $db = new DbOperations;
+	$chcrytpo = new ChCrytpo;
+
+	$request_data = $request->getParsedBody();
+
+	$EncKey = $request_data['EncKey'];
+
+	$MobileId = $chcrytpo->decrypt($request_data['MobileId']
+		, $EncKey); 
+
+	$user = $db->getUserByMobileId($MobileId);	
+
+	$aMobId = $chcrytpo->decrypt($request_data['AMobileId']
+		, $user['EncKey']);	
+
+	
+
+	$role = $chcrytpo->decrypt($request_data['Role']
+		, $EncKey); 	
+
+	//$auser = $db->getUserByMobileId($aMobId);
+
+	$result = $db->activateUser($aMobId, $role);
+
+
+	if($result == USER_ACTIVATED ){
+		$message = array();
+		$message['error'] = false;
+		$message['message'] = $chcrytpo->encrypt('User Activated successfully'
+		,$user['EncKey']); 
+	
+		$response->getBody()->write(json_encode($message));
+		return $response
+					->withHeader('Content-type', 'application/json')
+					->withStatus(200);
+	}
+
+	if($result == USER_NOT_ACTIVATED ){
+		$message = array();
+		$message['error'] = true;
+		$message['message'] = $chcrytpo->encrypt('Unable To Activate User'
+		,$user['EncKey']); 
+	
+		$response->getBody()->write(json_encode($message));
+		return $response
+					->withHeader('Content-type', 'application/json')
+					->withStatus(200);
+	}
+	return $response
+	->withHeader('Content-type', 'application/json')
+	->withStatus(422);
+
+});
+
+
+$app->post('/SearchOperation/Public/InActiveUsers', function (Request $request, Response $response) {
+    $db = new DbOperations;
+	$chcrytpo = new ChCrytpo;
+
+	$request_data = $request->getParsedBody();
+
+	$EncKey = $request_data['EncKey'];
+
+	$MobileId = $chcrytpo->decrypt($request_data['MobileId']
+		, $EncKey); 
+
+	$user = $db->getUserByMobileId($MobileId);
+
+	$users = $db->inActiveUsersList();
+
+	$response_data = array();
+
+	$response_data['userresult'] = $users;
+
+	$response->getBody()->write(json_encode(array('error'=>false, 'message'=>
+		$chcrytpo->encrypt(json_encode($response_data)
+	, $user['EncKey']))));
+			return $response
+					->withHeader('Content-type', 'application/json')
+					->withStatus(200);
+
+});
+
+
+function haveEmptyParameters($required_paras, $response){
+	$error = false;
+	$error_params = '';
+
+	$request_params = $_REQUEST;
+
+	foreach($required_paras as $param){
+		if(!isset($request_params[$param]) || strlen($request_params[$param]) <= 0){
+			$error = true;
+			$error_params .= $param . ', ';
+		}
+	}
+
+	if($error){
+		$error_detail = array();
+		$error_detail['error'] = true;
+		$error_detail['message'] = 'Required parameters ' 
+		. substr($error_params, 0, -2) 
+		. ' are missing or empty';
+	
+		$response->getBody()->write(json_encode($error_detail));
+		
+
+	}
+
+	return $error;
+}
+
 
 $app->run();
 
